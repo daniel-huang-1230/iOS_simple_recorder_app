87f9574a54d2c89a58875136133fdba0043b02e8
Daniel Huang <gn03249822@DanieltekiMacBook-Pro.local>
2017-07-14 14:21:26 +0800

    Camera works

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # iOS_simple_recorder_app
+
+I build this app to practice implementing the recording feature with the iPhone
+built-in camera.
--- SimpleRecorder/ViewController.swift
+++ SimpleRecorder/ViewController.swift
@@ -32,6 +32,35 @@ class ViewController: UIViewController,UIImagePickerControllerDelegate, UINaviga
     
     
     @IBAction func RecordAction(_ sender: UIButton) {
+        
+        //first check if the camera is available
+        
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.camera){
+            
+            print("Camera available!")
+            
+            let imagePicker=UIImagePickerController()
+            
+            imagePicker.delegate=self
+            
+            imagePicker.sourceType = .camera;
+            
+            imagePicker.mediaTypes=[kUTTypeMovie as String]
+            
+            //editing not allowed in this app
+            imagePicker.allowsEditing=false
+            
+            imagePicker.showsCameraControls=true
+            
+            self.present(imagePicker, animated:true,completion:nil)
+        
+        }
+        
+        else {
+            print("Camera unavailable")
+        }
+        
+        
     }
     
 
